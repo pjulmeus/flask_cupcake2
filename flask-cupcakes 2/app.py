4a74412c169654557527cb5123d540bf6752c140
pjulmeus <pjulmeus@gmail.com>
2023-10-04 01:42:15 -0400

    finished had bugs, struggled to complete

--- flask-cupcakes 2/app.py
+++ flask-cupcakes 2/app.py
@@ -1,1 +1,65 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, redirect, render_template, jsonify
+from models import db, connect_db, Cupcake
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcake_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/cupcakes')
+def cupcake_list():
+    all_cupcake = [c.serialize() for c in Cupcake.query.all()]
+    return jsonify(all_cupcake = all_cupcake)
+
+@app.route('/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake = cupcake.serialize())
+
+@app.route('/cupcakes/<int:cupcake_id>', methods = ["POST"])
+def create_cupcake():
+    if new_cupcake_flavor:
+        new_cupcake_flavor = request.json["flavor"]
+    if new_cupcake_size:
+        new_cupcake_size = request.json["size"]
+    if new_cupcake_rating:
+        new_cupcake_rating = request.json["rating"]
+    if new_cupcake_image:
+        new_cupcake_image = request.json["image"]
+    
+    new_cupcake = Cupcake(flavor = new_cupcake_flavor, size = new_cupcake_size, rating = new_cupcake_rating, image = new_cupcake_image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(new_cupcake.serialize())
+    return (response_json, 201)
+
+@app.route('/cupcakes/<int:cupcake_id>', methods = ["PATCH"])
+def edit_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    request.json.get('flavor', cupcake.flavor)
+    request.json.get('size', cupcake.size)
+    request.json.get('rating', cupcake.rating)
+    request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake = cupcake.serialize())
+
+@app.route('/cupcakes/<int:cupcake_id>', methods = ["PATCH"])
+def delete_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(cupcake)
+    db.session.commit(jsonify(message= "deleted"))
+
+
+
+
+
--- flask-cupcakes 2/models.py
+++ flask-cupcakes 2/models.py
@@ -1,1 +1,39 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+
+    # def __repr__(self):
+    #     p = self
+    #     return f" Hello my name is {p.first_name} {p.last_name}, here is my user picture {p.image_url}"
+    
+    # def get_first_name(cls, first_name):
+    #  return cls.query.filter(first_name)
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String,
+                     nullable=False,
+                     unique=True)
+    size = db.Column(db.String
+                     , nullable = False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, nullable=True, default='https://tinyurl.com/demo-cupcake')
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'flavor' : self.flavor,
+            'size' : self.size,
+            'rating' : self.rating, 
+            'image' : self.image
+        }
